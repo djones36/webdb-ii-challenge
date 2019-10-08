d776c29ee16b448ca4e90f24d859e46a06c4f215
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-08 12:40:31 -0700

    wip

--- API/server.js
+++ API/server.js
@@ -1,2 +1,13 @@
-const express = require('express');
-const
+const express = require("express");
+
+const server = express();
+const carRoutes = require("../routes/carRoutes");
+server.use(express.json());
+
+server.use("/api/cars", carRoutes);
+
+server.get("/", (req, res) => {
+  res.status(200).json("Successful Deployment");
+});
+
+module.exports = server;
